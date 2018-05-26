1a87fc84aa6f845fc3b7044b4cac0bdf15494417
blindcrikket <38725275+blindcrikket@users.noreply.github.com>
2018-05-26 15:02:11 -0300

    Add files via upload

---
+++ Assets/Core/CanvasManager.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class CanvasManager : MonoBehaviour {
+
+    public static CanvasManager instance;
+
+    public Text textScore;
+    public Text textMoney;
+    public Text textLife;
+
+    [Header("Traps Prices")]
+    public int priceBomb;
+
+    public GameObject prefabBomb;
+
+    public bool trap;
+
+    void Awake()
+    {
+        instance = this;
+    }
+
+    void Start () {
+        trap = false;
+    }
+
+    void Update ()
+    {  
+        
+
+        if (Input.GetMouseButtonDown(0) && trap){
+            if (GameManager.instance.money >= priceBomb){
+                GameManager.instance.money -= priceBomb;
+                UpdateHUD();
+                Instantiate(prefabBomb, Input.mousePosition, Quaternion.identity);
+                trap = false;
+            }
+        }
+    }
+
+    public void UpdateHUD()
+    {
+        textScore.text = "Score: " + GameManager.instance.score;
+        textMoney.text = "Money: " + GameManager.instance.money;
+        textLife.text = "Life: " + GameManager.instance.life;
+    }
+    
+
+    public void onButtonBombClick()
+    {
+        trap = true;
+    }
+
+}
